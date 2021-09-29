9034ab9e624fe854e46bc7b0382bbc7f441d63c0
Yohann Boniface <edhyjox@gmail.com>
2021-09-29 23:37:26 +0200

    :sparkles: Adding header

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,30 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
 </script>
-
 <template>
-  <img alt="Vue logo" src="assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Header/>
 </template>
 
 <style>
+:root {
+  --primary: #ffdd67;
+  --background: #121212;
+
+  --textInPrimary: #000;
+  --secondary: #ff5a79;
+  --secondaryShadow: #f3002e;
+}
+
+body {
+  background-color: var(--background);
+  margin: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Segoe UI, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: var(--textInPrimary)
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <h1>gitmoji Stats</h1>
+    <p id="description">Display the gitmoji frequency of your repositories</p>
+  </header>
+</template>
+
+<style scoped>
+header {
+  background-color: var(--primary);
+  padding: 4.5em 2em;
+  text-align: center;
+}
+
+h1 {
+  margin-block: 0;
+  font-size: 6em;
+}
+
+p#description {
+  font-size: 2em;
+  font-weight: bold;
+}
+</style>
